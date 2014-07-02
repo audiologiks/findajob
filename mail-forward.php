41cedad1a45264a23ae397585fab7750a54b3b22
Tom <editor@audiologiks.com>
2014-07-03 08:45:59 +0900

    Create mail-forward.php

---
+++ mail-forward.php
@@ -0,0 +1,11 @@
+<?php
+<Response>
+<Say voice="woman">Please leave a message after the tone.</Say>
+<Record maxLength="20" />
+</Response>
+<Response>
+<Record action="mail.php" />
+</Response>
+
+
+?>
