95755037d4912d969c24acfa621e75885ed77881
Tom <editor@audiologiks.com>
2014-07-03 08:46:46 +0900

    Create mail.php

---
+++ mail.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* This section ensures that Twilio gets a response.
+*/
+header('Content-type: text/xml');
+echo '<?xml version="1.0" encoding="UTF-8"?>';
+echo '<Response></Response>'; //Place the desired response (if any) here.
+/**
+* This section actually sends the email.
+*/
+$to = "tom.jones@audiologiks.com"; // Your email address.
+$subject = "Message from {$_REQUEST['From']}";
+$message = "You have received a message from {$_REQUEST['From']}.";
+$message .= "To listen to this message, please visit this URL: {$_REQUEST['RecordingUrl']}";
+$headers = "From: tom.jones@audiologiks.com"; // Who should it come from?
+mail($to, $subject, $message, $headers);
