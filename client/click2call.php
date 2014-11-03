bd05be7be3c13c10bbbd799a20f275b7cbf01cb3
Tom <editor@audiologiks.com>
2014-11-04 08:41:42 +0900

    to call a certain number

---
+++ client/click2call.php
@@ -0,0 +1,5 @@
+<h3>Please enter your phone number, and you will be connected to +81xxxxxxxx</h3>
+<form action="makecall.php" method="post">
+<span>Your Number: <input type="text" name="called" /></span>
+<input type="submit" value="Connect me!" />
+</form>
