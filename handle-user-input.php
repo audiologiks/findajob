59b3321474451ee9950ba9ce195548075cfe4417
Tom <editor@audiologiks.com>
2016-01-24 12:01:15 +0900

    Update with authentication
    added a simple access code for English and Japanese

--- handle-user-input.php
+++ handle-user-input.php
@@ -10,20 +10,41 @@
 	{
 		echo '<Redirect>http://safe-sea-5987.herokuapp.com/public/tk_server/clicktocall/misawa/hello-misawa.php</Redirect>';
 	}
+	else if ($user_pushed == 1)
+	{
+        echo '<Gather action="http://safe-sea-5987.herokuapp.com/public/tk_server/clicktocall/misawa/handle-candidate.php" numDigits="8">';
+		echo '<Say voice="alice" language="ja-JP">ありがとうございます。</Say>';
+		echo '<Say voice="alice" language="ja-JP">アクセスコードを入力してください。もしなかったら１０００を押してください</Say>';
+		echo '<Pause>';
+		echo '</Pause>';
+		echo '<Say voice="alice" language="ja-JP">サポート担当にをお話したいかた、3番を押してください</Say>';
+        echo '</Gather>';
+		echo '<Say>Sorry, I did not get that. Please enter the number again. すみません、も一度押してください。</Say>';
+        echo '<Redirect method="GET">http://safe-sea-5987.herokuapp.com/public/tk_server/clicktocall/misawa/handle-candidate-input.php"</Redirect>';
+	}
+	# @end snippet
 	# @start snippet
-	else if ($user_pushed == 20160124)
+	else if ($user_pushed == 2291968)
    { 
-	echo '<Gather action="http://safe-sea-5987.herokuapp.com/public/tk_server/clicktocall/misawa/web_jenny.php" numDigits="2">';
-	echo "<Say>Press the key code or the button on the web client for the word you want to listen to</Say>";
+	echo '<Gather action="http://safe-sea-5987.herokuapp.com/public/tk_server/clicktocall/misawa/02291968.php" numDigits="2">';
+	echo "<Say>Press the code or the button for the office or company you want to connect to</Say>";
 	echo '</Gather>';
-	echo '<Redirect method="GET">http://safe-sea-5987.herokuapp.com/public/tk_server/clicktocall/misawa/web_jenny.php</Redirect>';   
+	echo '<Redirect method="GET">http://safe-sea-5987.herokuapp.com/public/tk_server/clicktocall/misawa/02291968.php</Redirect>';   
    } 
    # @end snippet
 	# @end snippet
 	
-	else if ($user_pushed ==2)
+	 else if ($user_pushed == 2)
 	{
-     echo '<Redirect method="GET">http://safe-sea-5987.herokuapp.com/public/uk_server/prod01a/mail/menu-e.php</Redirect>';
+	echo '<Gather action="http://safe-sea-5987.herokuapp.com/public/tk_server/clicktocall/misawa/handle-student-e.php" numDigits="8">';
+    echo '<Say>Thank you for using the Twilio at Naka Meguro Meetup Services </Say>';
+	echo '<Say>Please enter your access code. If you have one, just press 1 0 0 0</Say>';
+	echo '<Pause>';
+	echo '</Pause>';
+	echo '<Say>To speak to a support representative, press 3</Say>';
+	echo '</Gather>';
+	echo '<Say>Sorry, I did not get that. Please enter the number again. すみません、も一度押してください。</Say>';
+    echo '<Redirect method="GET">http://safe-sea-5987.herokuapp.com/public/tk_server/clicktocall/misawa/handle-student-e.php"</Redirect>';
     }
 	# @start snippet
 	else if ($user_pushed == 3)
@@ -37,22 +58,21 @@
 		# @start snippet
 	else if ($user_pushed == 5)
 	{
-		echo '<Say>Welcome to the Language Exchange .</Say>';
-		echo '<Say>To create your own set of words, just send us a text file with the words separated by commas</Say>';
-		echo '<Say>Click How to on the page to find instructions how to submit your own word sets</Say>';
-		echo '<Say>To record words, please listen for the beep. Recordings are kept for 7 days and then deleted. Please contact us at the meetup to register for this app.</Say>';
-		echo '<Say>Please record your words and phrases. You have 30 seconds for each</Say>';
-		echo '<Record maxLength="30" action="http://www.audiologiks.com/m/kddiweb/handle-candidate-recording-e.php"/>';
+		echo '<Say>Welcome to the Misawa Directory by Tom.</Say>';
+		echo '<Say>We include information on local businesses accessible by phone or p c</Say>';
+		echo '<Say>Stay tuned. We will update as we get more information.</Say>';
+			
+		echo '<Redirect>http://safe-sea-5987.herokuapp.com/public/tk_server/clicktocall/misawa/hello-misawa.php</Redirect>';
 	
 	}
 	# @end snippet
-	else if ($user_pushed == 6)
+	else if ($user_pushed == 4)
 	{
 		echo '<Play>https://s3-ap-northeast-1.amazonaws.com/all-jp-1/audio/8611/RE841184c9f0be7861fa938b30ceca80ee.wav</Play>';
 
 	}
 	# @end snippet
-	else if ($user_pushed == 1)
+	else if ($user_pushed == 6)
 	{
 		echo '<Play>https://s3-ap-northeast-1.amazonaws.com/all-jp-1/audio/8611/RE841184c9f0be7861fa938b30ceca80ee.wav</Play>';
 
