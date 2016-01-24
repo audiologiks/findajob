6597fb68531ac45d7141f32e239e47acccfc71c4
Tom <editor@audiologiks.com>
2016-01-24 13:20:29 +0900

    adding access numbers
    added access numbers 1000 -1011

--- handle-recording.php
+++ handle-recording.php
@@ -1,10 +1,111 @@
 <?php
-
-	header("content-type: text/xml");
-	echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+	header('Content-type: text/xml');
+	echo '<?xml version="1.0" encoding="UTF-8"?>';
+    echo '<Response>';
+    $user_pushed = (int) $_REQUEST['Digits'];
+      # @start snippet
+   if ($user_pushed == 1000) 
+   {
+    echo '<Gather action="http://safe-sea-5987.herokuapp.com/public/tk_server/clicktocall/misawa/handle-candidate-question.php" numDigits="8">';
+    echo '<Say voice="alice" language="ja-JP">ピン　コードを入力ください</Say>';
+    echo '</Gather>';
+    echo '<Redirect method="GET">http://safe-sea-5987.herokuapp.com/public/tk_server/clicktocall/misawa/handle-candidate-question.php</Redirect>';
+     } 
+   # @end snippet
+    # @start snippet
+   elseif ($user_pushed == 1001) 
+   { 
+    echo '<Gather action="http://safe-sea-5987.herokuapp.com/public/tk_server/clicktocall/misawa/handle-candidate-question.php" numDigits="8">';
+    echo '<Say voice="alice" language="ja-JP">ピン　コードを入力ください</Say>';
+    echo '</Gather>';
+    echo '<Redirect method="GET">http://safe-sea-5987.herokuapp.com/public/tk_server/clicktocall/misawa/handle-candidate-question.php</Redirect>';
+     } 
+   # @end snippet
+    elseif ($user_pushed == 1002) 
+   { 
+    echo '<Gather action="http://safe-sea-5987.herokuapp.com/public/tk_server/clicktocall/misawa/handle-candidate-question.php" numDigits="8">';
+    echo '<Say voice="alice" language="ja-JP">ピン　コードを入力ください</Say>';
+    echo '</Gather>';
+    echo '<Redirect method="GET">http://safe-sea-5987.herokuapp.com/public/tk_server/clicktocall/misawa/handle-candidate-question.php</Redirect>';
+     }
+   # @end snippet
+        # @start snippet
+   if ($user_pushed == 1003) 
+   {
+    echo '<Gather action="http://safe-sea-5987.herokuapp.com/public/tk_server/clicktocall/misawa/handle-candidate-question.php" numDigits="8">';
+    echo '<Say voice="alice" language="ja-JP">ピン　コードを入力ください</Say>';
+    echo '</Gather>';
+    echo '<Redirect method="GET">http://safe-sea-5987.herokuapp.com/public/tk_server/clicktocall/misawa/handle-candidate-question.php</Redirect>';
+     } 
+   # @end snippet
+    # @start snippet
+   elseif ($user_pushed == 1004) 
+   { 
+    echo '<Gather action="http://safe-sea-5987.herokuapp.com/public/tk_server/clicktocall/misawa/handle-candidate-question.php" numDigits="8">';
+    echo '<Say voice="alice" language="ja-JP">ピン　コードを入力ください</Say>';
+    echo '</Gather>';
+    echo '<Redirect method="GET">http://safe-sea-5987.herokuapp.com/public/tk_server/clicktocall/misawa/handle-candidate-question.php</Redirect>';
+     } 
+   # @end snippet
+    elseif ($user_pushed == 1005) 
+   { 
+    echo '<Gather action="http://safe-sea-5987.herokuapp.com/public/tk_server/clicktocall/misawa/handle-candidate-question.php" numDigits="8">';
+    echo '<Say voice="alice" language="ja-JP">ピン　コードを入力ください</Say>';
+    echo '</Gather>';
+    echo '<Redirect method="GET">http://safe-sea-5987.herokuapp.com/public/tk_server/clicktocall/misawa/handle-candidate-question.php</Redirect>';
+     } 
+    echo '<Say voice="alice" language="ja-JP">すみません、も一度押してください。</Say>';
+         # @start snippet
+   if ($user_pushed == 1006) 
+   {
+    echo '<Gather action="http://safe-sea-5987.herokuapp.com/public/tk_server/clicktocall/misawa/handle-candidate-question.php" numDigits="8">';
+    echo '<Say voice="alice" language="ja-JP">ピン　コードを入力ください</Say>';
+    echo '</Gather>';
+    echo '<Redirect method="GET">http://safe-sea-5987.herokuapp.com/public/tk_server/clicktocall/misawa/handle-candidate-question.php</Redirect>';
+     } 
+   # @end snippet
+    # @start snippet
+   elseif ($user_pushed == 1007) 
+   { 
+    echo '<Gather action="http://safe-sea-5987.herokuapp.com/public/tk_server/clicktocall/misawa/handle-candidate-question.php" numDigits="8">';
+    echo '<Say voice="alice" language="ja-JP">ピン　コードを入力ください</Say>';
+    echo '</Gather>';
+    echo '<Redirect method="GET">http://safe-sea-5987.herokuapp.com/public/tk_server/clicktocall/misawa/handle-candidate-question.php</Redirect>';
+     } 
+   # @end snippet
+    elseif ($user_pushed == 1008) 
+   { 
+    echo '<Gather action="http://safe-sea-5987.herokuapp.com/public/tk_server/clicktocall/misawa/handle-candidate-question.php" numDigits="8">';
+    echo '<Say voice="alice" language="ja-JP">ピン　コードを入力ください</Say>';
+    echo '</Gather>';
+    echo '<Redirect method="GET">http://safe-sea-5987.herokuapp.com/public/tk_server/clicktocall/misawa/handle-candidate-question.php</Redirect>';
+     }
+   # @end snippet
+        # @start snippet
+   if ($user_pushed == 1009) 
+   {
+    echo '<Gather action="http://safe-sea-5987.herokuapp.com/public/tk_server/clicktocall/misawa/handle-candidate-question.php" numDigits="8">';
+    echo '<Say voice="alice" language="ja-JP">ピン　コードを入力ください</Say>';
+    echo '</Gather>';
+    echo '<Redirect method="GET">http://safe-sea-5987.herokuapp.com/public/tk_server/clicktocall/misawa/handle-candidate-question.php</Redirect>';
+     } 
+   # @end snippet
+    # @start snippet
+   elseif ($user_pushed == 1010) 
+   { 
+    echo '<Gather action="http://safe-sea-5987.herokuapp.com/public/tk_server/clicktocall/misawa/handle-candidate-question.php" numDigits="8">';
+    echo '<Say voice="alice" language="ja-JP">ピン　コードを入力ください</Say>';
+    echo '</Gather>';
+    echo '<Redirect method="GET">http://safe-sea-5987.herokuapp.com/public/tk_server/clicktocall/misawa/handle-candidate-question.php</Redirect>';
+     } 
+   # @end snippet
+    elseif ($user_pushed == 1011) 
+   { 
+    echo '<Gather action="http://safe-sea-5987.herokuapp.com/public/tk_server/clicktocall/misawa/handle-candidate-question.php" numDigits="8">';
+    echo '<Say voice="alice" language="ja-JP">ピン　コードを入力ください</Say>';
+    echo '</Gather>';
+    echo '<Redirect method="GET">http://safe-sea-5987.herokuapp.com/public/tk_server/clicktocall/misawa/handle-candidate-question.php</Redirect>';
+     } 
+    echo '<Say voice="alice" language="ja-JP">すみません、も一度押してください。</Say>';
+    echo '</Response>';
 ?>
-<Response>
-	<Say>Thanks for leaving a message... please listen to it or hangup.</Say>
-	<Play><?php echo $_REQUEST['RecordingUrl']; ?></Play>
-	<Say>Goodbye.</Say>
-</Response>
