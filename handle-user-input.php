832f6713cbc4478f5298d0b2d5444fcafd624d60
Tom <editor@audiologiks.com>
2016-01-24 09:20:42 +0900

    Update handle-user-input.php

--- handle-user-input.php
+++ handle-user-input.php
@@ -3,167 +3,60 @@
 	echo '<?xml version="1.0" encoding="UTF-8"?>';
 
 	echo '<Response>';
-
+    
 	$user_pushed = (int) $_REQUEST['Digits'];
 
 	if ($user_pushed == 0)
 	{
-		echo '<Redirect>http://www.audiologiks.com/tmp/audio/recording/recording/hello-monkey-handle-key.php</Redirect>';
+		echo '<Redirect>http://safe-sea-5987.herokuapp.com/public/tk_server/clicktocall/misawa/hello-misawa.php</Redirect>';
 	}
 	# @start snippet
-	else if ($user_pushed == 1)
-	{
-		echo '<Gather action="http://www.audiologiks.com/tmp/audio/recording/recording/handle-extension.php" numDigits="1">';
-		echo "<Say>Please enter your party's extension.</Say>";
-		echo '<Say>Press 0 to return to the main menu</Say>';
-		echo '</Gather>';
-		echo "<Say>Sorry, I didn't get your response.</Say>";
-		echo '<Redirect method="GET">http://www.audiologiks.com/tmp/audio/recording/recording/hello-monkey-handle-key.php</Redirect>';
-	}
+	else if ($user_pushed == 20160124)
+   { 
+	echo '<Gather action="http://safe-sea-5987.herokuapp.com/public/tk_server/clicktocall/misawa/web_jenny.php" numDigits="2">';
+	echo "<Say>Press the key code or the button on the web client for the word you want to listen to</Say>";
+	echo '</Gather>';
+	echo '<Redirect method="GET">http://safe-sea-5987.herokuapp.com/public/tk_server/clicktocall/misawa/web_jenny.php</Redirect>';   
+   } 
+   # @end snippet
 	# @end snippet
 	
-	else if ($user_pushed == 2)
+	else if ($user_pushed ==2)
 	{
-		echo '<Say>Record your message after the tone.</Say>';
-	   echo '<Record maxLength="30" action="http://www.audiologiks.com/tmp/audio/recording/hello-monkey-handle-recording.php"/>';
-	}
+     echo '<Redirect method="GET">http://safe-sea-5987.herokuapp.com/public/uk_server/prod01a/mail/menu-e.php</Redirect>';
+    }
 	# @start snippet
 	else if ($user_pushed == 3)
 	{
-		echo '<Play>http://www.audiologiks.com/tmp/audio/recording/recording/welcome-june.mp3</Play>';
-		echo '<Play>http://www.audiologiks.com/tmp/audio/recording/recording/salesforce.wav</Play>';
+		echo '<Say>Connecting you to Support.</Say>';
+		echo '<Dial record="true" callerId="+819072840430">';
+		echo '<Client>jenny</Client>';
+		echo '</Dial>';
 	}
 	# @end snippet
-	else if ($user_pushed == 4)
+		# @start snippet
+	else if ($user_pushed == 5)
 	{
-		echo '<Play>http://www.audiologiks.com/tmp/audio/recording/recording/welcome-june.mp3</Play>';
-		echo '<Play>http://www.audiologiks.com/sites/default/files/linkedinq1.wav</Play>';
+		echo '<Say>Welcome to the Language Exchange .</Say>';
+		echo '<Say>To create your own set of words, just send us a text file with the words separated by commas</Say>';
+		echo '<Say>Click How to on the page to find instructions how to submit your own word sets</Say>';
+		echo '<Say>To record words, please listen for the beep. Recordings are kept for 7 days and then deleted. Please contact us at the meetup to register for this app.</Say>';
+		echo '<Say>Please record your words and phrases. You have 30 seconds for each</Say>';
+		echo '<Record maxLength="30" action="http://www.audiologiks.com/m/kddiweb/handle-candidate-recording-e.php"/>';
 	
 	}
-	# @start snippet
-   elseif ($user_pushed == 5) 
-   { 
-   echo "<Say>Your recording will start soon</Say>";
-	echo '<Play>http://www.audiologiks.com/tmp/audio/recording/recording/ciena-quarter.wav</Play>	';
-   } 
-   # @end snippet
-  elseif ($user_pushed == 6)
-  { 
-  echo "<Say>Your recording will start soon</Say>";
-	echo '	<Play>/tmp/audio/recording/recording/LibertyMedia/LibertyMediaQ1Results.mp3</Play>	';
-  } 
-  elseif ($user_pushed == 7)
-  { 
-  echo "<Say>Your recording will start soon</Say>";
-	echo '	<Play>/tmp/audio/att.wav</Play>	';
-	} 
-  elseif ($user_pushed == 88) 
-  { 
-   echo "<Say>Your recording will start soon</Say>";
-	echo '<Play>http://www.audiologiks.com/tmp/audio/recording/recording/alcoa-quarter.wav</Play>';
-  } 
-   elseif ($user_pushed == 9) 
-  { 
-    echo "<Say>Your recording will start soon</Say>";
-	echo '<Play>/tmp/audio/recording/recording/Medidata/14-04-08-22-20-31.wav</Play>';
-   } 
-   elseif ($user_pushed == 27) 
-  { 
-    echo "<Say>Your recording will start soon</Say>";
-	echo '<Play>/tmp/audio/recording/recording/SAIC/SAIC-Earnings-Part1.mp3</Play>';
-   } 
-   elseif ($user_pushed == 10) 
-   { 
-    echo "<Say>Your recording will start soon</Say>";
-	echo '<Play>/tmp/audio/recording/recording/Teradata/teradata-qr.mp3</Play>';
+	# @end snippet
+	else if ($user_pushed == 6)
+	{
+		echo '<Play>https://s3-ap-northeast-1.amazonaws.com/all-jp-1/audio/8611/RE841184c9f0be7861fa938b30ceca80ee.wav</Play>';
 
-   } 
-   elseif ($user_pushed == 11) 
-   { 
-    echo "<Say>Your recording will start soon</Say>";
-	echo '<Play>/tmp/audio/recording/recording/VMWARE/14-03-30-23-25-01.wav</Play>';
+	}
+	# @end snippet
+	else if ($user_pushed == 1)
+	{
+		echo '<Play>https://s3-ap-northeast-1.amazonaws.com/all-jp-1/audio/8611/RE841184c9f0be7861fa938b30ceca80ee.wav</Play>';
 
-   } 
-   elseif ($user_pushed == 12) 
-   { 
-    echo "<Say>Your recording will start soon</Say>";
-	echo '<Play>/tmp/audio/recording/recording/aol.wav</Play>';
- } 
- elseif ($user_pushed == 13) 
- { 
-echo "<Say>Your recording will start soon</Say>";
-	echo '<Play>/tmp/audio/recording/recording/trw.wav</Play>';
- } 
- elseif ($user_pushed == 123456) 
- { 
-echo "<Say>Your recording will start soon</Say>";
-	echo '<Play>/tmp/audio/recording/recording/verint.wav</Play>';
-} 
-elseif ($user_pushed == 14) 
-{ 
-echo "<Say>Your recording will start soon</Say>";
-	echo '<Play>/tmp/audio/recording/recording/bnymellon.wav</Play>';
- } 
- elseif ($user_pushed == 15) 
- { 
-echo "<Say>Your recording will start soon</Say>";
-	echo '<Play>/tmp/audio/recording/recording/test2.wav</Play>';
- } 
- elseif ($user_pushed == 16) 
- { 
-echo "<Say>Your recording will start soon</Say>";
-	echo '<Play>/tmp/audio/recording/recording/test3.mp3</Play>';
- } 
- elseif ($user_pushed == 17) 
- { 
-   echo "<Say>Your recording will start soon</Say>";
-	echo '<Play>/tmp/audio/recording/recording/test4.wav</Play>';
- } 
- elseif ($user_pushed == 18) 
- { 
-echo "<Say>Your recording will start soon</Say>";
-	echo '<Play>/tmp/audio/recording/recording/test5.wav</Play>';
- } 
- elseif ($user_pushed == 19) 
- { 
-echo "<Say>Your recording will start soon</Say>";
-echo '<Play>/tmp/audio/recording/recording/test6.wav</Play>';
- } 
- elseif ($user_pushed == 20) 
- { 
-echo "<Say>Your recording will start soon</Say>";
-echo '<Play>/tmp/audio/recording/recording/test7.wav</Play>';
- } 
- elseif ($user_pushed == 21) 
- { 
-echo "<Say>Your recording will start soon</Say>";
-echo '<Play>/tmp/audio/recording/recording/test8.wav</Play>';
- } 
- elseif ($user_pushed == 22) 
- { 
-echo "<Say>Your recording will start soon</Say>";
-echo '<Play>/tmp/audio/recording/recording/test9.wav</Play>';
- } 
- elseif ($user_pushed == 23) 
- { 
-echo "<Say>Your recording will start soon</Say>";
-echo '<Play>/tmp/audio/recording/recording/test10.wav</Play>';
- } 
- elseif ($user_pushed == 24) 
- { 
-echo "<Say>Your recording will start soon</Say>";
-echo '<Play>/tmp/audio/recording/recording/test11.wav</Play>';
- } 
- elseif ($user_pushed == 25) 
- { 
-echo "<Say>Your recording will start soon</Say>";
-echo '<Play>/tmp/audio/recording/recording/tenneco.wav</Play>';
- } 
- elseif ($user_pushed == 26) 
- { 
-echo "<Say>Your recording will start soon</Say>";
-echo '<Play>/tmp/audio/recording/recording/test13.wav</Play>';
-}
-
+	}
+	# @end snippet
 	echo '</Response>';
 ?>
