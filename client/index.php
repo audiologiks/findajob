09af3efa90c885e8decfc018e7b98a58088eaae3
Tom <editor@audiologiks.com>
2018-08-08 20:20:25 +0900

    index file
    for a recruitment/training web app

---
+++ client/index.php
@@ -0,0 +1,203 @@
+<?php
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Searching for Jobs</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+
+<!-- start header -->
+<head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+<link rel="stylesheet" href="https://bootswatch.com/4/sandstone/bootstrap.min.css">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <meta name="google-site-verification" content="F93N0Z3VogATWfhNpMtrQvJ0XKwtRKqgFZt6JZixpzE" />
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+  
+</head>
+<body>
+<!-- end header -->
+<!-- start page -->
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+      <a class="navbar-brand" href="/">Audiologiks</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+          </li>
+         <li class="nav-item">
+            <a class="nav-link" href="https://audiologiks.zendesk.com">FAQ</a>
+         </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/ja">日本語</a>
+         </li>
+          <li class="nav-item">
+            <a class="nav-link" href="https://www.audiologiks.com/webclient/Japan/desk_app.php">Desktop App</a>
+         </li>
+         <li>
+         </li>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
+<!-- Audiologiks -->  
+  <main role="main">          
+<!-- Main jumbotron for a primary marketing message or call to action -->
+<div class="jumbotron">
+<div class="container">
+ <img src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/calendar/icons8-calendar-8.png"><img src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/all/icons8-torii.png"><img src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/japan.png">
+	<h3>Tokyo, Japan</h3>
+    <iframe src="http://free.timeanddate.com/clock/i6ax1m83/n248/tct/pct/avb/tt0/tw0/tm1/ts1/tb4" align="bottom" frameborder="0" width="87" height="34" allowTransparency="true"></iframe>
+
+	
+	<h1>Find a Job in Japan or Overseas</h1>
+
+    <h3>Check out our Knowledge Base of Articles on Getting a Job in Japan or Overseas </h3> 
+    <p>Click on the icon in the lower right<img src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/IT/icons8-topic-50.png"> and type in your questions .</p>
+  
+ <h2>Audiologiks supports individuals in their ongoing career development. </h2>
+ 
+ <div class="searchbox" align="right">
+<script>
+  (function() {
+    var cx = '016459681525703113343:cu3o6sthpfg';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
+</script>
+<gcse:search></gcse:search>
+</div>
+  
+  <p>We organize Meetups so people can meet and exchange information about methods, ideas and leads on career steps to finding jobs and building skills for careers. Chat with us to ask questions.</p>
+  
+<h1>Give Yourself a Reward for Completing Tasks during Your Job Search</h1>
+<h3>Score your daily activities to find a job. Have you prepared your resume, researched what kind of job you want and the salary you want?</h3><p> Keep score and try to keep your daily activity score at the level you want to find a job.</p>
+<ul>
+<div class="row">
+<div class="col-sm-4" style="background-color:lavender;">
+<table>
+<tbody>
+<tr><th>Points</th> <th>Task</th> <th>Icon</th> </tr>
+<tr><td> 150 </td><td><a target="_blank" href="https://audiologiks.zendesk.com/hc/ja/articles/115000903412-Meetup-Notes-August-2017-Global-Careers-Information-">Talk to a Contact about a Job</a> </td><td> <a target="_blank" title="Look at our Github Page" href="https://github.com/audiologiks/kb-Articles/blob/master/Images/icons/office.md"><img src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/office/icons8-contact-50.png"></a> </td><tr>
+<tr><td> 100 </td><td> <a target="_blank" href="https://audiologiks.zendesk.com/hc/ja/articles/115001611566-Job-Boards-Bilingual">Search for a Job</a> </td><td> <a title="Look at our Github Page" target="_blank" href="https://audiologiks.zendesk.com/hc/ja/articles/115001611566-Job-Boards-Bilingual"><img src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/office/icons8-job-seeker.png"></a> </td><tr>
+<tr><td>10</td><td> <a target="_blank" href="https://drive.google.com/drive/u/0/folders/0B6Wg9avpDlA7dFpYTG9fWl9CcG8">Download Resume</a></td><td> <img src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/office/icons8-download-resume.png"> </td><tr>
+<tr><td>10	</td><td> <a target="_blank" href="https://www.linkedin.com/jobs/">Apply for a Job </a></td><td> <img src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/office/icons8-submit-resume-filled-50.png"> </td><tr>
+<tr><td>10	</td><td> <a target="_blank" href="http://localhost/firstdemo/al-firstdemo/recruitrewards/index.html">Fill out a Form</a> </td><td> <img src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/office/icons8-clipboard-checklist.png"></td><tr>
+<tr><td>10	</td><td><a target="_blank" href="https://docs.google.com/document/d/166PnCYe04FTZS_JRrm_IYBbZiYfJ8VqSG99BaShWnRU/edit?usp=sharing">Get Latest copy of the resume</a> </td><td> <img src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/office/icons8-permanent-job.png"></td><tr>
+</tbody>
+</table>
+</div>
+
+<div class="col-sm-4" style="background-color:lavenderblush;">
+<table>
+<tbody>
+<tr><th>Points</th> <th>Task</th> <th>Icon</th> </tr>
+<tr><td>20	</td><td> <a target="_blank" href="https://audiologiks.zendesk.com/hc/ja/articles/216485943-Bilingual-Recruiters-List-of-36-Companies-from-Outsourcing-to-Recruitment">Send email, text or IM to recruiter</a> </td><td> <img src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/office/icons8-email.png"></a></td><tr>
+<tr><td>10	</td><td> <a target="_blank" href="https://www.linkedin.com/jobs/">Apply over Linkedin</a> </td><td> <img src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/office/icons8-linkedin-48.png"></td><tr>
+<tr><td>10	</td><td> <a target="_blank" href="http://calendar.google.com">Make an appointment</a></td><td> <img src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/office/icons8-calendar-with-ok-sign.png"></td><tr>
+<tr><td>10	</td><td> <a target="_blank" href="http://www.skype.com">Make a phone call</a> </td><td> <img src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/office/icons8-skype.png"></td><tr>
+<tr><td>10	</td><td> <a target="_blank" href="https://audiologiks.zendesk.com/hc/ja/community/posts/115000795411-Current-Features-of-the-Platform">Get a call back</a> </td><td> <img src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/dialpad/original/icons8-phone.png"></td><tr>
+<tr><td>10	</td><td> <a target="_blank" href="https://audiologiks.zendesk.com/hc/ja/articles/115003157403-Practice-Job-Interviews">Have an interview</a> </td><td> <img src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/office/icons8-interview.png"></td><tr>
+</tbody>
+</table>
+</div>
+    
+<div class="col-sm-4" style="background-color:lavender;">
+<table>
+<tbody>
+<tr><th>Points</th> <th>Task</th> <th>Icon</th> </tr>
+<tr><td>10	</td><td> <a target="_blank" href="https://top-us.com/register">Upload Resume</a> </td><td> <img src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/office/icons8-send-job-list.png"></td><tr>
+<tr><td>50	</td><td> <a target="_blank" href="https://drive.google.com">Write Cover Letter (English)</a> </td><td> <img src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/office/icons8-task-50.png"></td><tr>
+<tr><td>10	</td><td> <a target="_blank" href="https://www.google.co.jp/search?q=join+our+talent+community&oq=join+our+talent+community&aqs=chrome..69i57.11175j0j4&sourceid=chrome&ie=UTF-8">Joined a Talent Community</a> </td><td> <img src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/office/icons8-video-interview.png"></td><tr>
+<tr><td>10	</td><td> <a target="_blank" href="https://audiologiks.zendesk.com/hc/ja/articles/215114926-International-and-Japanese-Job-Boards">Used a Job Portal</a></td><td> <img src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/office/icons8-job.png"></td><tr>
+<tr><td>50	</td><td> <a target="_blank" href="">Rewrote resume for job spec</a> </td><td> <img src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/office/more+office/Office+50/icons8-hand-with-pen.png"></td><tr>
+<tr><td>10	</td><td> <a target="_blank" href="https://audiologiks.zendesk.com/hc/ja/articles/216485163-What-Recruiters-are-Looking-for">Identify new roles</a> </td><td> <img src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/dialpad/original/icons8-contacts-2.png"></td><tr>
+</tbody>
+</table>
+</ul>
+<h4>We are raising money via Indiegogo</h4>
+      <a href="https://igg.me/at/audiologiks1/x/17973408"> <img height="75" width="75" src="../../../../../images/web/IGG_Heart.png"></a>
+    <p>We are offering different web apps for users and Meetup members to access our content, practice and help them learn. Check out our perks to add features to the basic web client, and please donate to our campaign.</p>
+
+<p><a class="btn btn-primary btn-lg" href="/JpJobs/JobBoards/" role="button">Learn more &raquo;</a></p>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- al-kb-leaderboard -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:728px;height:90px"
+     data-ad-client="ca-pub-0436801005616261"
+     data-ad-slot="5967170806"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+</div>
+</div>
+<div class="container">
+<!-- Example row of columns -->
+<div class="row">
+<div class="col-md-4" align="right" >
+	<h2><strong>Sites</strong></h2>
+		<p><a href="http://recruitcall.slack.com"><img height="50" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/logos/cos/%E3%83%97%E3%83%AC%E3%82%BB%E3%83%B3/sharing/slack_square.png"></a><a href="https://trello.com/"><img height="50" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/logos/cos/%E3%83%97%E3%83%AC%E3%82%BB%E3%83%B3/sharing/trello.png"></a><a href="http://drive.google.com"><img height="50" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/logos/gdrive.png"></a><a href="http://www.gmail.com"><img height="50" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/logos/gmail.png"></a><a href="http://www.zendesk.com"><img height="50" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/logos/zendesk_1.png"></a>
+		</p>
+	<h3>Platform</h3>
+		<p><a target="_blank" title="Twilio US" href="http://www.twilio.com"><img height="50" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/logos/twilio-us.png"></a><a target="_blank" title="Twilio Jp" href="http://twilio.kddi-web.com"><img height="50" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/logos/twilio-jp.png"></a><a href="www.heroku.com"><img height="50" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/logos/heroku.png"></a><a href="www.loggly.com"><img height="50" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/logos/loggly.png"></a><a href="aws.amazon.com"><img height="50" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/logos/cos/aws.png"></a><a href="http://www.cloudflare.com"><img height="50" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/logos/cloudflare.png"></a><a href="http://www.icons8.com"><img height="50" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/logos/icons8.png"><a href="http://www.paypal.com"><img height="50" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/logos/paypal.jpeg"></a></p>
+		
+</div>
+<div class="col-md-4" align="right" >
+	<h2><strong>Events</strong></h2>
+	<p><a href="https://www.meetup.com"><img height="50" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/logos/meetup.png"></a><a href="https://www.doorkeeper.jp/"><img height="50" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/logos/doorkeeper.png"></a></p>
+	
+	<h3>Other Ways of Working in Japan</h3>
+	<p><a href="https://audiologiks.zendesk.com/hc/ja/articles/115003946352-Links-to-Information-on-Different-Visa-Types-in-Japan"><img height="50" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/logos/logo-work.png"></a><a href="https://www.youtube.com/watch?v=iFzTo5qsxMI&list=PLEOsyUjbMDYU2g-oP8JPfy99UgCtyYVRh"><img height="50" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/logos/youtube.png"></a><a href="https://soundcloud.com/tom-jones-110/sets"><img height="50" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/logos/soundcloud.jpg"></a></p>
+	    <h3>Analytics and Marketing</h3>
+		<p><a target="_blank" title="mailchimp" href="http://www.mailchimp.com"><img height="50" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/logos/mailchimp.png"></a><a target="_blank" title="google analytics" href="http://analytics.google.com"><img height="50" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/logos/ganalytics.jpeg"></a><a href="www.bitly.com"><img height="50" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/logos/bitly.jpeg"></a><a href="www.hootsuite.com"><img height="50" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/logos/hootsuite.png"></a><a href="http://www.surveymonkey.com"><img height="50" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/logos/surveymonkey.png"></a></p>
+		   <h3>Features</h3>
+	<p><a href="www.audiologiks.com"><img src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/icons/group_2.png"></a></p>
+	
+</div>
+<div class="col-md-4" align="right">
+	<h2><strong>Links</strong></h2>
+	<p><a href="https://line.me/en/"><img height="50" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/logos/line.jpg"></a><a href="https://www.ameba.jp/"><img height="50" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/logos/ameba-logo.jpg"></a></p>
+	<h3>Social Media</h3>
+	<p><a href="https://www.facebook.com/audiologiks"><img height="50" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/logos/cos/facebook.png"></a><a href="www.pinterest.com"><img height="50" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/logos/pinterest.png"></a><a href="www.twitter.com"><img height="50" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/logos/twitter.png"></a></p>
+	<h3>Skill Building</h3>
+	<p><a href="https://audiologiks.zendesk.com/hc/ja/articles/115003308086-Definitions-of-Different-Levels-in-the-JLPT-"><img width="150" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/logos/jlpt-logo.gif"></a><a href="https://github.com/audiologiks/kb-Articles"><img height="50" src="https://s3-ap-northeast-1.amazonaws.com/all-jp-1/img/logos/github.png"></a></p>
+ </div>
+</div> 
+<!-- /container -->
+<p><a href="#">Back to top</a></p>
+</div>
+    </main>
+    <footer class="container">
+     <p>Copyright 2015 - 2018 <a href="https://www.audiologiks.com/">Audiologiks</a><a href="al-firstdemo/privacy.html">Privacy</a> | <a href="al-firstdemo/Beta_Program_Terms_and_Conditions.php">Beta program Terms</a></p> 
+    </footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
+  </body>
+</html>
